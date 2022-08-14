9dda8b90e4b4bc9dc7ecb1d48ac47bf6c84da73f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-15 00:51:16 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }:AppProps){
+  return(
+    <ThemeProvider attribute='class'>
+      <Component {...pageProps}></Component>
+    </ThemeProvider>
+  )
 }
+
 
 export default MyApp
